9e6c6761e4e5a808e66670d0576b0118e87c733c
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-09-26 14:52:26 +0100

    Remove unused col-md-10 classes.

--- search.php
+++ search.php
@@ -27,30 +27,28 @@
 				<div class="post-list">
 								
 					<div class="container-fluid">	
-						<div class="row">
-							<div class="col-md-10 col-md-offset-1">
-								<?php while ( have_posts() ) : the_post();
-								$date = get_the_date('l - jS F - Y');
-								 ?>	
+						
+						<?php while ( have_posts() ) : the_post();
+						$date = get_the_date('l - jS F - Y');
+						 ?>	
+
+						<article <?php post_class(); ?>>
+							<h4><?php the_title(); ?></h4>
+							
+							<?php if ($post->post_type == 'post') { ?>
+							<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><i class="fa fa-calendar"></i> <?php echo $date; ?></time>
+							<?php } ?>
+									
+							<?php the_excerpt(); ?>	
+								
+							<a href="<?php esc_url( the_permalink() ); ?>" title="View <?php the_title_attribute(); ?> article" class="btn btn-default btn-block" rel="bookmark">
+								View <?php echo ($post->post_type == 'post') ? "Post":"Page"; ?> <i class="fa fa-arrow-circle-right fa-lg"></i>
+							</a>
+							
+						</article>
 		
-								<article <?php post_class(); ?>>
-									<h4><?php the_title(); ?></h4>
-									
-									<?php if ($post->post_type == 'post') { ?>
-									<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><i class="fa fa-calendar"></i> <?php echo $date; ?></time>
-									<?php } ?>
-											
-									<?php the_excerpt(); ?>	
-										
-									<a href="<?php esc_url( the_permalink() ); ?>" title="View <?php the_title_attribute(); ?> article" class="btn btn-default btn-block" rel="bookmark">
-										View <?php echo ($post->post_type == 'post') ? "Post":"Page"; ?> <i class="fa fa-arrow-circle-right fa-lg"></i>
-									</a>
-									
-								</article>
-				
-								<?php endwhile; ?>
-							</div>
-						</div>
+						<?php endwhile; ?>
+							
 					</div>	
 
 				</div><!-- End List -->
