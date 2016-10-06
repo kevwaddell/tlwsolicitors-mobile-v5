40b8dc59a320f3332e5b65bb90e24466332676c1
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-10-06 14:11:29 +0100

    Fix search pop up.

--- searchform.php
+++ searchform.php
@@ -2,17 +2,16 @@
 <?php if (is_home() || is_archive() || is_single()) { ?>
 
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<div class="input-group">	
 		
-		<input type="hidden" name="post_type" value="post" />
+	<input type="hidden" name="post_type" value="post" />
+	
+	<input type="search" value="<?php the_search_query(); ?>" placeholder="Search…" class="search-query form-control" name="s" id="s" />
+	
 		
-		<span class="input-group-btn">
-		<input type="submit" id="searchsubmit" value="&#xf002;" class="search-submit btn btn-default" />
-		</span>
-		
-		<input type="search" value="<?php the_search_query(); ?>" placeholder="Search…" class="search-query form-control" name="s" id="s" />
+	<div class="search-btn-wrap">
+	 <button type="submit" id="searchsubmit" class="search-submit">Start search<i class="fa fa-angle-right fa-lg btn-pointer"></i> </button>
+	</div>
   	
-  	</div>
 </form>
 
 <?php } else { ?>
@@ -22,7 +21,7 @@
   <input type="search" value="<?php the_search_query(); ?>" placeholder="Search…" class="form-control search-query" name="s" id="s" />
   
   <div class="search-btn-wrap">
-  	<input type="submit" id="searchsubmit" value="Start search" class="search-submit" /><i class="fa fa-angle-right fa-lg btn-pointer"></i>
+	  <button type="submit" id="searchsubmit" class="search-submit">Start search<i class="fa fa-angle-right fa-lg btn-pointer"></i> </button>
   </div>
 </form>	
 	
