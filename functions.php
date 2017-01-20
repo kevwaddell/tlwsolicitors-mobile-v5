5c7c7650fc51d488c86350ad51143d269af735a3
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-20 10:09:15 +0000

    ‘defer’ js instead of ’async defer’

--- functions.php
+++ functions.php
@@ -46,9 +46,16 @@ function add_async_attribute($tag, $handle) {
    'functions'
    );
    
+    if ($handle == 'gform_json' || $handle == 'gform_gravityforms' || $handle == 'gform_conditional_logic' || $handle == 'gform_placeholder') {
+	$scripts_to_asyc[] = 'gform_json';   
+	$scripts_to_asyc[] = 'gform_gravityforms';
+	$scripts_to_asyc[] = 'gform_conditional_logic';
+	$scripts_to_asyc[] = 'gform_placeholder';
+	}
+   
     foreach($scripts_to_asyc as $asyn_script) {
       if ($asyn_script === $handle) {
-         return str_replace(' src', ' async defer src', $tag);
+         return str_replace(' src', ' defer src', $tag);
       }
    }
    return $tag;
