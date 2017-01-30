debaa89fa69750a7943851b2f04241ae22d797f6
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-30 15:30:00 +0000

    New defer script test.

--- footer.php
+++ footer.php
@@ -29,23 +29,20 @@
 				
 		<?php wp_footer(); ?>
 		
-<!--
 		<script>
-      var loadDeferredStyles = function() {
-        var addStylesNode = document.getElementById("deferred-styles");
-        var replacement = document.createElement("div");
-        replacement.innerHTML = addStylesNode.textContent;
-        document.body.appendChild(replacement);
-        addStylesNode.parentElement.removeChild(addStylesNode);
-        document.body.classList.remove("atfc-mobile-css");
-      };
-      var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
-      if (raf) { 
-	      raf(function() { window.setTimeout(loadDeferredStyles, 0);});
-     } else { window.addEventListener('load', loadDeferredStyles); }
-    </script>
--->
+	      var loadDeferredStyles = function() {
+	        var addStylesNode = document.getElementById("deferred-styles");
+	        var replacement = document.createElement("div");
+	        replacement.innerHTML = addStylesNode.textContent.replace("none","all");
+	        document.body.appendChild(replacement);
+	        addStylesNode.parentElement.removeChild(addStylesNode);
+	        document.body.classList.remove("atfc-mobile-css");
+	      };
+	      var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+	      if (raf) { 
+		      raf(function() { window.setTimeout(loadDeferredStyles, 0);});
+	     } else { window.addEventListener('load', loadDeferredStyles); }
+    	</script>
 
-	    
 	</body>
 </html>
