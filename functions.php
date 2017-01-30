f98612cdca5dbfb2f2f3087a667dfdbe6e1c3743
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-30 15:45:27 +0000

    New defer script test.

--- functions.php
+++ functions.php
@@ -163,7 +163,7 @@ if (!is_admin()) {
 		    $dom->loadHTML($html);
 		    $a = $dom->getElementById($handle.'-css');	
 			//return "<script>loadCSS('" . $a->getAttribute('href') . "',document.getElementById('loadcss'),'" . $a->getAttribute('media') . "');</script>\n";
-			return "<noscript id=\"deferred-styles\"><link rel=\"". $a->getAttribute('rel') ."\" type=\"text/css\" href=\"".$a->getAttribute('href')."\" media=\"none\"/></noscript>";
+			return "<noscript id=\"deferred-styles\"><link rel=\"". $a->getAttribute('rel') ."\" type=\"text/css\" href=\"".$a->getAttribute('href')."\" media=\"none\" onload=\"if(media!='all')media='all'\"></noscript>";
 			//return "<noscript id=\"deferred-styles\"><link rel=\"". $a->getAttribute('rel') ."\" type=\"text/css\" href=\"".$a->getAttribute('href')."\"/></noscript>";
 			}
 	
